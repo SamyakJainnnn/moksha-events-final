a422fd903a0fea61d382704afba583294d5f4bdd
Samyak Jain <86378744+SamyakJainnnn@users.noreply.github.com>
2022-03-10 13:52:24 +0530

    Add files via upload

---
+++ cardlist.jsx
@@ -0,0 +1,72 @@
+import img from "../images/pic.png"
+
+const cards = [
+    {
+        key: 1,
+        heading: "Cam sniper",
+        time: "Nescii, 2:00",
+        paragraph: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quis excepturi modi asperiores beatae. Delectus a labore neque laudantium beatae!",
+        img: img,
+
+    },
+
+    {
+        key: 2,
+        heading: "Game Hub",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 3,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 4,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 5,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 6,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 7,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 8,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+    {
+        key: 9,
+        heading: "event1",
+        time: "2:00",
+        paragraph: "jdjdj djdsjs sjsjjsxx djsjxjx sjsxjxsjj",
+        img: img
+    },
+
+];
+
+export default cards;
